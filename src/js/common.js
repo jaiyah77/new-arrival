4ebaea689a122db10cc4a122cafd1680c71079a5
jaiyah77 <jaiyah1977@gmail.com>
2016-12-29 12:43:28 +0900

    add source

---
+++ src/js/common.js
@@ -0,0 +1,11 @@
+// Called Swiper
+!function () {
+	var swiper = new Swiper('.new_arrival_container', {
+		pagination: '.pagination',
+		nextButton: '.button-next',
+		prevButton: '.button-prev',
+		slidesPerView: 1,
+		paginationClickable: true,
+		loop: true
+	});
+}();
